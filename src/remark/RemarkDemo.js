04ddaff0b86ef5daaf205f6a375df6b703612519
Alexander Rusanov <alexander.rusanov@gmail.com>
2023-11-08 17:21:26 +0000

    Update MarkdocDemo.js and RemarkDemo.js

--- src/remark/RemarkDemo.js
+++ src/remark/RemarkDemo.js
@@ -1,60 +1,47 @@
-import { useEffect, useState } from 'react';
-import example from './content/example.markdoc?raw';
-import Editor from '@monaco-editor/react';
-import Markdoc from '@markdoc/markdoc'
-import React from 'react';
-import schemaConfig from './schema';
-import components from './blocks';
-import 'react-tabs/style/react-tabs.css';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { useEffect, useState } from "react";
+import example from "./content/example.markdoc?raw";
+import Markdoc from "@markdoc/markdoc";
+import React from "react";
+import schemaConfig from "./schema";
+import components from "./blocks";
+import "react-tabs/style/react-tabs.css";
+import { DemoStand } from "../DemoStand";
 
+export function RemarkDemo() {
+  const [value, setValue] = useState(example);
+  const [data, setData] = useState({});
+  const { ast, errors, renderTree, content } = data;
 
+  useEffect(() => {
+    const ast = Markdoc.parse(value);
+    const errors = Markdoc.validate(ast, schemaConfig);
+    const renderTree = Markdoc.transform(ast, schemaConfig);
+    const content = Markdoc.renderers.react(renderTree, React, { components });
+    setData({ ast, errors, renderTree, content });
+  }, [value]);
 
-export function MarkdocDemo() {
-    const [value, setValue] = useState(example);
-    const [ast, setAst] = useState({});
-    const [renderTree, setRenderTree] = useState({});
-    const [errors, setErrors] = useState([]);
-    const [reactTree, setReactTree] = useState(undefined);
-
-    useEffect(() => {
-        const ast = Markdoc.parse(value);
-        const errors = Markdoc.validate(ast, schemaConfig);
-        const transform = Markdoc.transform(ast, schemaConfig);
-        const reactTree = Markdoc.renderers.react(transform, React, { components })
-        setAst(ast);
-        setRenderTree(transform);
-        setErrors(errors);
-        setReactTree(reactTree);
-    }, [value])
-    return (
-        <main>
-            <div style={{ display: 'flex', flexBasis: '50%' }}>
-                <Tabs style={{ width: '100%' }}>
-                    <TabList>
-                        <Tab>Markdoc</Tab>
-                        <Tab>AST</Tab>
-                        <Tab>Render AST</Tab>
-                    </TabList>
-                    <TabPanel>
-                        <Editor height="90vh" defaultLanguage="markdown" defaultValue={value} defaultPath='example.markdoc' onChange={(value) => setValue(value)} />
-                    </TabPanel>
-                    <TabPanel>
-                        <Editor height="90vh" defaultLanguage="json" value={JSON.stringify(ast, undefined, ' ')} defaultPath='parsed tree' options={{ readOnly: true }} />
-                    </TabPanel>
-                    <TabPanel>
-                        <Editor height="90vh" defaultLanguage="json" value={JSON.stringify(renderTree, undefined, ' ')} defaultPath='render tree' options={{ readOnly: true }} />
-                    </TabPanel>
-                </Tabs>
-                <div style={{ padding: '10px', borderLeft: 'solid 2px #444', height: '100vh', overflow: 'scroll', width: '100%' }}>
-                    {errors.length > 0 && <pre style={{ background: '#fa8e8e', color: '#fff', border: 'solid 2px #f00', padding: 10 }}>
-                        Errors:<br />
-                        {JSON.stringify(errors, undefined, ' ')}
-                    </pre>
-                    }
-                    {reactTree}
-                </div>
-            </div>
-        </main>
-    );
+  return (
+    <DemoStand
+      tabs={["Markdoc", "AST", "Render AST"]}
+      onChange={(value) => setValue(value)}
+      values={[
+        {
+          language: "markdown",
+          value: example,
+        },
+        {
+          language: "json",
+          value: JSON.stringify(ast, undefined, " "),
+          readOnly: true,
+        },
+        {
+          language: "json",
+          value: JSON.stringify(renderTree, undefined, " "),
+          readOnly: true,
+        },
+      ]}
+      content={content}
+      errors={errors}
+    />
+  );
 }
